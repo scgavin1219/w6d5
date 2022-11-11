8839e3c169d815481b3fe32682ea458dbe0e8c43
Noam Z <nzoam93@gmail.com>
2022-11-11 15:23:18 -0800

    added cats

--- ninety-nine-cats/app/models/cat.rb
+++ ninety-nine-cats/app/models/cat.rb
@@ -1,2 +1,28 @@
 class Cat < ApplicationRecord
+    require 'action_view'
+    include ActionView::Helpers::DateHelper
+
+    CAT_COLORS = %w(red orange yellow green blue purple tortoise)
+
+    validates :birth_date, :color, :name, :sex, presence: true
+    validates :color, inclusion: {in: CAT_COLORS, message: "invalid color"}
+    validates :sex, inclusion: {in: %w(M F)}
+    validate :birth_date_cannot_be_in_the_future
+
+    def birth_date_cannot_be_in_the_future
+        if birth_date.present? && birth_date > Date.today #no nil error
+            errors.add(:birth_date, "can't be in the future!!!!!!L:JDJFKLDSFDKFJKDLFSJ")
+        end
+    end
+
+    def age
+        Date.today.year - birth_date.year
+    end
+
+
+
+
+
+
+
 end
--- ninety-nine-cats/db/seeds.rb
+++ ninety-nine-cats/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ApplicationRecord.transaction do
+    # Destroy tables (not necessary if using `rails db:seed:replant`)
+    puts 'Destroying tables...'
+    Cat.destroy_all
+
+    # Reset primary keys (i.e., ids)
+    puts 'Resetting id sequences...'
+    %w(cats).each do |table_name|
+      ApplicationRecord.connection.reset_pk_sequence!(table_name)
+    end
+
+    # Create seed data
+    puts 'Creating seed data...'
+    c1 = Cat.create!(birth_date: "2015/01/20", color: "tortoise", name: "burger", sex: "F")
+    c2 = Cat.create!(birth_date: "2011/02/23", color: :red, name: :clifford, sex: :F)
+
+    puts 'Done!'
+  end
